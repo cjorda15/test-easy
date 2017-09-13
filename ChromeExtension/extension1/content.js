bb83d62f40a1bd33338e855f4757b1ba72602562
unknown <doopdon>
2017-09-12 19:43:25 -0600

    inintal setup

---
+++ ChromeExtension/extension1/content.js
@@ -0,0 +1,6 @@
+var gan = JSON.parse(window.localStorage.getItem('person'))|| {name:"gan",age:12}
+console.log(gan.name +" is "+ gan.age)
+gan.age+=1
+window.localStorage.setItem('person',JSON.stringify(gan))
+console.log(document);
+ 
