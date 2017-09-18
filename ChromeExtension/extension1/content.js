690fd04a26b14edf0f83ca7d0994a503cb0411e8
Chris Jordan <crobertjordan@yahoo.com>
2017-09-18 13:12:33 -0600

    fudge

--- ChromeExtension/extension1/content.js
+++ ChromeExtension/extension1/content.js
@@ -1,6 +1,9 @@
-var gan = JSON.parse(window.localStorage.getItem('person'))|| {name:"gan",age:12}
-console.log(gan.name +" is "+ gan.age)
-gan.age+=1
-window.localStorage.setItem('person',JSON.stringify(gan))
+var gan = JSON.parse(window.localStorage.getItem('person')) || {
+  name: 'gan',
+  age: 12
+};
+console.log(gan.name + ' is ' + gan.age);
+gan.age += 1;
+window.localStorage.setItem('person', JSON.stringify(gan));
 console.log(document);
- 
+console.log('fuck');
