fe58ca0175a36506c19d299a17487befb5aed769
Doopdon <gannonhuiting@gmail.com>
2017-09-21 16:18:51 -0600

    gave background all element info

--- ChromeExtension/extention2/background.js
+++ ChromeExtension/extention2/background.js
@@ -1,29 +1,9 @@
-function hey()
-{
-	console.log("hey");
-}
 
 console.log("runnin background.js")
-// console.log(chrome.tabs);
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-  	console.log(request)
-  	//var obj = JSON.parse(request.data);
-  	//console.log("start:",obj,":end");
+  	var parser = new DOMParser();
+    var doc = parser.parseFromString(request.elementInfo, "text/xml");
+  	console.log(doc);
   });
-
-
-// function hey()
-// {
-// 	alert("hey");
-// }
-// Called when the user clicks on the browser action.
-// chrome.browserAction.onClicked.addListener(function(tab) {
-//   // Send a message to the active tab
-//   console.log("clicked");
-//   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-//     var activeTab = tabs[0];
-//     chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
-//   });
-// });
--- ChromeExtension/extention2/myscript.js
+++ ChromeExtension/extention2/myscript.js
@@ -1,21 +1,72 @@
 
-function myfunc(){
-
-	console.log(document);
-
-
+function startListener(){
 	document.addEventListener('click',(event)=>{
-		
-		var id = event.target.id;
-		console.log("target",event.path);
-
-		testFunction({message:"test1",data:id});
+		var o = getReleventInfo(event);
+		sendMessageToBackground(o);
 	})
 
 }
 
-function testFunction(object){
+function sendMessageToBackground(object){
 	chrome.extension.sendMessage(object);
 }
 
-$(document).ready(myfunc);
+function getReleventInfo(event)
+{
+	console.log(event.target.innerHTML);
+
+	var objInfo = {type:"element",
+	path:processPath(event.path),
+	elementInfo:event.target.innerHTML}
+	return objInfo;
+}
+
+function getObjectInfo(target)
+{
+	rtnObj = {};
+	rtnObj.id = target.id;
+	rtnObj.class = target.className;
+	return rtnObj;
+}
+
+function processPath(path)
+{
+	var partPath = [];
+ 	var part = path[0];
+	if("HTMLBodyElement|HTMLHtmlElement|HTMLHtmlElement|Window".includes(elemType(part)))
+	{
+		//do nothing for now
+	}
+	else
+	{
+		partPath.push(getObjectInfo(part));
+		//console.log(partPath)
+		path.splice(0, 1);
+		partPath = partPath.concat(processPath(path));
+	}
+	return partPath;
+}
+
+function logOutPath(path)
+{
+	outputList =[];
+	path.forEach(function(val)
+	{
+		outputList.push(elemType(val));
+	});
+	console.log(outputList);
+}
+
+
+function elemType(elementObj)
+{
+	//"HTMLElement",
+	//"HTMLDivElement",
+	//"HTMLBodyElement",
+	//"HTMLHtmlElement",
+	//"HTMLHtmlElement", 
+	//"Window"
+	return elementObj.toString().split(" ")[1].split("]")[0];
+}
+
+$(document).ready(startListener);
